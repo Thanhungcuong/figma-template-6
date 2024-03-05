363e3bbe706907e78091829a77f4974aede09731
Thanhungcuong <78573263+Thanhungcuong@users.noreply.github.com>
2024-03-05 10:42:38 +0700

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,19 @@ module.exports = {
   content: [],content: ["index.html"],
   theme: {
     extend: {
+      gradients: (theme) => ({
+        'colorpn': `linear-gradient(90deg, ${theme('colors.green.500')} 0%, ${theme('colors.blue.500')} 100%)`,
+      }),
       zIndex: {
         '100': '100',
-      }
-      
+      },
+      colors: {
+        'gray88': '#888888',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-gradients'),
+  ],
 }
 
